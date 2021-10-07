babceb4fdcd0500ad26d7576fa5e054864886bcd
René Föhring <rf@bamaru.de>
2021-10-07 08:44:10 +0200

    Merge

--- mix.exs
+++ mix.exs
@@ -12,11 +12,11 @@ defmodule PhoenixHtmlSanitizer.Mixfile do
         maintainers: ["René Föhring"],
         licenses: ["MIT"],
         links: %{
-         "GitHub" => "https://github.com/elixirstatus/phoenix_html_sanitizer",
+          "GitHub" => "https://github.com/elixirstatus/phoenix_html_sanitizer"
         }
       ],
-      build_embedded: Mix.env == :prod,
-      start_permanent: Mix.env == :prod,
+      build_embedded: Mix.env() == :prod,
+      start_permanent: Mix.env() == :prod,
       deps: deps
     ]
   end
@@ -39,7 +39,7 @@ defmodule PhoenixHtmlSanitizer.Mixfile do
   # Type `mix help deps` for more examples and options
   defp deps do
     [
-      {:phoenix_html, "~> 2.0"},
+      {:phoenix_html, "~> 2.0 or ~> 3.0"},
       {:html_sanitize_ex, "~> 1.3.0"},
       {:inch_ex, ">= 0.0.0", only: :docs}
     ]
